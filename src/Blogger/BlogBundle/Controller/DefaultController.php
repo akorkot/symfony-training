09345cef6c205f93ec37430b6e216cab74cdad0c
akorkot <ayoub.korkot@gmail.com>
2016-12-07 14:16:10 +0000

    mise en place de symblog

---
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Default:index.html.twig');
+    }
+}
