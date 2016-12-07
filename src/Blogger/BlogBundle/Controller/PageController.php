f93b055548d30f20b4b0edaa3acdb062eb9fe4ef
akorkot <ayoub.korkot@gmail.com>
2016-12-07 17:20:27 +0000

    Page de contact : validateurs, formulaires et envoi d’emails

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -1,9 +1,11 @@
 <?php
-// src/Blogger/BlogBundle/Controller/PageController.php
 
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+
 
 class PageController extends Controller
 {
@@ -12,11 +14,40 @@ class PageController extends Controller
         return $this->render('BloggerBlogBundle:Page:index.html.twig');
     }
 
-
 	public function aboutAction()
     {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
 
+	public function contactAction()
+	{
+	    $enquiry = new Enquiry();
+	    $enquiryType = new EnquiryType();
 
+	    $form = $this->createForm($enquiryType, $enquiry);
+
+	    $request = $this->getRequest();
+
+
+
+
+	    if ($request->getMethod() == 'POST') {
+	        $form->bindRequest($request);
+
+	    print "<pre>";
+	    print_r($enquiry);
+	    die;
+	        if ($form->isValid()) {
+	            // Perform some action, such as sending an email
+
+	            // Redirect - This is important to prevent users re-posting
+	            // the form if they refresh the page
+	            return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+	        }
+	    }
+
+	    return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+	        'form' => $form->createView()
+	    ));
+	}
 }
